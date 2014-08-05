5f1fd9f243e24b5237e1eb83c6857b67f7476648
Sam Schaack <samschaack1@gmail.com>
2014-08-04 20:43:55 -0700

    Update new_post_comment.js

--- app/assets/javascripts/views/new_post_comment.js
+++ app/assets/javascripts/views/new_post_comment.js
@@ -2,7 +2,6 @@ Sync.Views.NewPostComment = Backbone.View.extend({
   template: JST["comments/new_post_comment"],
   
   initialize: function(options) {
-    // this.commentId = options.commentId;
   },
   
   render: function() {
