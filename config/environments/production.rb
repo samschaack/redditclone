ca4440508a4f7fc158f97bfdf45f5d9e5e53db0f
App Academy Student <student@appacademy.io>
2014-08-04 22:09:46 -0700

    removed commented out code from all over project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   config.action_dispatch.default_headers = {
-      'X-Frame-Options' => 'ALLOWALL'
+    'X-Frame-Options' => 'ALLOWALL'
   }
   
   # Do not fallback to assets pipeline if a precompiled asset is missed.
